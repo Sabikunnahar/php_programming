63f673e2d7fa51b6ac793d87aa5ac9fa7afc9881
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2024-09-04 12:52:41 +0600

    [Update]
    [Update]

--- array_json.php
+++ array_json.php
@@ -4,15 +4,36 @@ $devs =[
     [
         'id' => 1,
         'name'  => 'bristy',
+        'age'  => 26,
         'address' => 'motijheel'
     ],
     [
         'id' => 2,
         'name'  => 'Kamrul',
-        'address' => 'motijheel'
+        'age'  => 27,
+        'address' => 'Shamoly'
+    ],
+    [
+        'id' => 3,
+        'name'  => 'Sadaf',
+        'age'  => 28,
+        'address' => 'panthapath'
+    ],
+    [
+        'id' => 3,
+        'name'  => 'Asif',
+        'age'  => 29,
+        'address' => 'Banaree'
     ],
 
 ];
 foreach ($devs as $data) {
-    echo "<h1>{$data['name']} </h1>";
+    if ($data['age'] >27) {
+        echo "<h4>{$data['id']} </h4>";
+        echo "<h4>{$data['name']} </h4>";
+        echo "<h4>{$data['age']} </h4>";
+        echo "<h4>{$data['address']} </h4>";
+        echo "<hr>";
+    }
+
 }
